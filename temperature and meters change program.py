301c9510b7bc2de34970f880d2d00d8570974e35
kiran_kumar <86544378+Chennakiran2004@users.noreply.github.com>
2023-11-02 21:47:15 +0530

    Add files via upload

---
+++ temperature and meters change program.py
@@ -0,0 +1,11 @@
+temperature_in_celcius=int(input())
+temperature_in_celcius=(temperature_in_celcius*9/5)+32
+print("Temperature_in_farenheit: "+str(temperature_in_celcius)+" "+"F")
+
+temperature_in_farenheit=int(input())
+temperature_in_farenheit=(temperature_in_farenheit-32)*5/9
+print("Temperature_in_celcius: "+str(temperature_in_farenheit)+" "+"C")
+
+Heigth_in_cm=int(input())
+Height_in_cm=(Heigth_in_cm)*0.0328084
+print("Height_in_Feet: "+str(Height_in_cm))
